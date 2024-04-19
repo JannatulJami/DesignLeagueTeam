0d4be0f32ddea432c32c21b7c430c4da5be2aa2f
DavidMuratov <144723035+DavidMuratov@users.noreply.github.com>
2024-04-19 14:17:08 -0400

    Update David.js

--- David/David.js
+++ David/David.js
@@ -9,3 +9,10 @@ window.addEventLister('load', () => {
     document.body.style.overflow = 'initial';
   }, 1000);
 });
+
+const coinSfx = new Sfx('sfx/Mario Coin.wav');
+const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
+const powerUpSfx = new Sfx('sfx/Mario Powerup.wav');
+const container = document.querySelector('.content');
+let ninja;
+
