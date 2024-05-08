ce311def303d41416c0c1fc47060fc25683951c1
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2024-05-08 08:29:47 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,42 @@
 const container = document.querySelector('.game');
+
+var character = {
+  x:50,
+  y: canvas.height-70,
+  width:40,
+  height:60,
+  jumping:false,
+  jumpHeight:20,
+  jumpSpeed:10,
+  gravity:1
+};
+ 
 var charcterImg = newImage();
-charcterImg.src = "game_img/
+charcterImg.src = "game_img/cat.jpg";
+
+function updateCharcter() {
+  if (charcter.jumping){
+    charcter.y -= charcter.jumpSpeed-= charcter.gravity;
+  
+    if (character.y >= canvas.height - charcter.jumpHeight) {
+      character.jumping = false;
+      character.jumpSpeed = 5;
+      character.y  = canvas.height - 70;
+    }
+  }
+}
+
+//function to jump
+function jump() {
+  if(!character.jumping){
+    character.jumping = true;
+  }
+}
+
+//event listeener for jumping 
+function (event) {
+if (event.code === "u")
+  jump();
+}
+});
+  
